1ee09e564a2dc1ef51e9871cb84d6cd897e7e720
Ankit Kumar <drankitkr011@gmail.com>
2025-11-08 22:47:59 +0530

    Created controllers/Auth.js and Created SendOtp Functionality

---
+++ server/controllers/Auth.js
@@ -0,0 +1,53 @@
+const OTP = require('../models/Otp');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const Profile = require('../models/Profile');
+const otpGenerator = require('otp-generator');
+const becrypt = require('bcrypt');
+const {mailSender} = require('../utils/mailSender');
+const {passwordUpdated} = require('../mail/templates/passwordUpdate');
+require('dotenv').config();
+
+const generateOtp = async() =>{
+    const otp = otpGenerator.generate(6,{
+        upperCaseAlphabets:false,
+        lowerCaseAlphabets:false,
+        specialChars:false
+    });
+    const existingOtp = await OTP.findOne({otp});
+    if(existingOtp){
+        return await generateOtp();
+    }
+    return otp;
+}
+
+const sendotp = async(req,res,next)=>{
+    try{
+        const {email} = req.body;
+        console.log(email);
+
+        const isUserExist = await User.findOne({email:email});
+        if(isUserExist){
+            return res.status(401).json({
+                success:false,
+                message:"User with this email already exists"
+            })
+        }
+
+        const otp = await generateOtp();
+
+        await OTP.create({email,otp});
+        return res.status(200).json({
+            success:true,
+            message:"OTP sent successfully",
+            otp:otp
+        })
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:"Something went wrong while sending otp"
+        })
+    }
+}
